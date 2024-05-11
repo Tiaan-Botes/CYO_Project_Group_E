173f298a333a6dc33e2603c6e111b0a1f1781311
Sean Botha <seanbotha2003@gmail.com>
2024-05-11 16:53:35 +0200

    Update model.py

---
+++ src/model.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import matplotlib.pyplot as plt 
+import plotly.express as px
+import datetime as dt
+from datetime import timedelta, date
+
+#Just to get a range for data, subject to change
+start_date = date.today() - timedelta(days=365*9)
+end_date = date.today() + timedelta(days=365*1)
+
+#Basic data to just get a basic database
+df = yf.download('AAPL', start=start_date, end=end_date)
+
+plot_data = df.loc[start_date:end_date]
+fig = px.line(
+    data_frame=plot_data,
+    x=plot_data.index,
+    y=['Adj Close', 'ma_20', 'ma_50'])
+
+fig.show()
